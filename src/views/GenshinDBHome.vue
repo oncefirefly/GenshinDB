3e5f963df6466a502a90ba14938743d19a14ea42
oncefirefly <oncefirefly@gmail.com>
2023-03-09 17:00:00 +0300

    home page added

---
+++ src/views/GenshinDBHome.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="mx-4 sm:mx-auto sm:w-10/12 w-full flex items-center flex-col">
+    <h1
+      class="text-white text-center justify-self-center uppercase text-xl font-bold"
+    >
+      welcome! <br />
+      this is home page
+    </h1>
+  </section>
+</template>
