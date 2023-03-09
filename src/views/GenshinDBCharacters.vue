5d4ae1cc79fd3e8f4357a769152adfc12b8541bd
oncefirefly <oncefirefly@gmail.com>
2023-03-09 17:02:10 +0300

    characters page added

---
+++ src/views/GenshinDBCharacters.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useCharactersListStore } from "@/stores/CharactersListStore";
+
+const charactersListStore = useCharactersListStore();
+</script>
+
+<template>
+  <div v-for="character of charactersListStore.charactersList" :key="character">
+    {{ character }}
+  </div>
+</template>
